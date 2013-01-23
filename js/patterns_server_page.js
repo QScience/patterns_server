46c9f83fdf7b3b3ee4f0c6bfb20464c81124ced5
ray <xrfind@gmail.com>
2013-01-23 12:30:09 +0100

    change row'id attribute to row'class attribute; download times grown every download by JS

--- js/patterns_server_page.js
+++ js/patterns_server_page.js
@@ -1,15 +1,12 @@
 jQuery(document).ready(function() {
-    var pattern_row = jQuery('#pattern_row_1');
-    //alert(pattern_row.length);
-    //alert(pattern_row.length);
-    var s = "#pattern_row_1";
-    var download_link = jQuery("s .download-link");
-    var download_times = jQuery('#pattern_row_1 .download-times');
-    //alert(download_link.parents("#pattern_row_1").find(".download-times").text());
+    
+    //when click on download link, download times grown by 1.
+    var download_link = jQuery(".pattern_row .download-link");
     download_link.click(function() {
-    //    alert(this.parents("#pattern_row_1").find(".download-times").text());
-        //var numb = Number(download_times.text()) + 1; 
-        //download_times.html(numb);
-       // return false;
+        var download_times = jQuery(this).parent().next();
+        var numb = Number(download_times.text()) + 1; 
+        download_times.children().text(numb);
     });
+
+    //
 });
