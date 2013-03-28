89ce7b9d79e32485ab3ba359c690ed2c1d44ff28
ray <xrfind@gmail.com>
2013-03-28 03:23:23 +0100

    theme

--- js/patterns_server_page.js
+++ js/patterns_server_page.js
@@ -27,35 +27,17 @@ jQuery(document).ready(function() {
         return false;
     });
 
-    //Rate
-    //var rate_link = jQuery(".pattern_row .rate-link");
-    //rate_link.click(function() {
-	//	if (jQuery(this).text() == "Like") {
-	//		jQuery(this).text("Unlike");
-	//		var rate_times = jQuery(this).parent().prev().prev();
-	//		var numb = Number(rate_times.text()) + 1; 
-	//		rate_times.children().text(numb);
-	//	}
-	//	else {
-	//		jQuery(this).text("Like");
-	//		var rate_times = jQuery(this).parent().prev().prev();
-	//		var numb = Number(rate_times.text()) - 1; 
-	//		rate_times.children().text(numb);
-	//	}
-
-    //    var url_div = jQuery(this).attr("href");
-    //    url_div += " #one_pattern_div";
-    //    jQuery("#one_pattern_div").load(url_div); 
-    //    return false;
-    //});
-	
-	//search box
+    //search box
 	jQuery('.span9 input#edit-search').addClass('span7');
 	jQuery('.span12 input#edit-search').addClass('span10');
 
-	//button for back or detail
-	//alert(jQuery('#all_patterns_div').siblings('#one_pattern_div').find('.server_detail').attr('class'));
-	//var server_detail = jQuery('#all_patterns_div').siblings('#one_pattern_div').find('.server_detail');
-	//server_detail.text('Detail');
-	//server_detail.attr('href', server_detail.attr('name'));
+	//server or detail link
+	var server_detail = jQuery('#all_patterns_div').attr('id');
+	if (server_detail!='all_patterns_div') {
+		var sd = jQuery('#one_pattern_div').find('.server_detail');
+		sd.text('Home');
+		sd.attr('href', sd.attr('name'));
+	}
+	
+
 });
